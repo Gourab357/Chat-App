168b166d0f8029dbdb18cff0aafd42d2584ebdef
Gourab Mahato <gourabmahato009@gmail.com>
2024-07-16 22:31:57 +0530

    Update index.ts

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,10 +3,19 @@ import SocketService from "./services/socket";
 
 async function init(){
     const socketService= new SocketService();
-
-    const httpServer=http.createServer();
+    const requestHandler = (req, res) => {
+        if (req.method === 'GET' && req.url === '/') {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Server running');
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Not Found');
+        }
+    };
+    const httpServer = http.createServer(requestHandler);
     const PORT = process.env.PORT ? process.env.PORT : 8000;
 
+    
     socketService.io.attach(httpServer);
 
     httpServer.listen(PORT, () => console.log(`HTTP Server started at PORT: ${PORT}`));
